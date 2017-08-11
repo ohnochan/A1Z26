88aa22b1e63ebe930ee9e30bec86e6faae84695e
ohnochan <dev.ohnochan@icloud.com>
2017-08-11 22:57:56 +0300

    Add files via upload

---
+++ A1Z26.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ *
+ *
+ * Copyright (C) 2017 OhNoChan
+ * GitHub: http://github.com/ohnochan
+ *
+ *
+ **/
+
+function A1Z26_Encode ($str, $abc)
+{
+    $new_str = "";
+
+    $abc = preg_split("//u", $abc, -1, PREG_SPLIT_NO_EMPTY);
+    $arr_str = preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+
+    array_unshift($abc, " ");
+
+    for ($i = 0; $i < count($arr_str); $i++)
+    {
+        if ($arr_str[$i] != $abc[0])
+        {
+            if ($i > 0 && $arr_str[$i - 1] != $abc[0])
+            {
+                $new_str .= "-";
+            }
+
+            $new_str .= array_search($arr_str[$i], $abc);
+        }
+        else
+        {
+            $new_str .= $abc[0];
+        }
+    }
+
+    return $new_str;
+}
+
+function A1Z26_Decode ($str, $abc)
+{
+    $new_str = "";
+    $temp = "";
+
+    $abc = preg_split("//u", $abc, -1, PREG_SPLIT_NO_EMPTY);
+    $arr_str = preg_split("//u", $str, -1, PREG_SPLIT_NO_EMPTY);
+
+    array_unshift($abc, " ");
+
+    for ($i = 0; $i < count($arr_str); $i++)
+    {
+        if ($arr_str[$i] == "-")
+        {
+            continue;
+        }
+
+        if ($arr_str[$i] == $abc[0])
+        {
+            $new_str .= $abc[0];
+            continue;
+        }
+
+        $temp .= $arr_str[$i];
+
+        if (!is_numeric($arr_str[$i + 1]))
+        {
+            $new_str .= $abc[$temp];
+            $temp = "";
+        }
+    }
+
+    return $new_str;
+}
+?>
